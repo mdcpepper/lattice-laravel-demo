397a4b49cd51a96fc8d00ed9dbf81d0fab40dbe2
Mike Pepper <mdcpepper@gmail.com>
2026-02-16 04:49:58 +0000

    Add GitHub Actions tests and migrate to Pest (#5)

---
+++ tests/Feature/IndexTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Feature;
+
+test("the home page returns a successful response", function () {
+    $response = $this->get("/");
+
+    $response->assertStatus(200);
+});
---
+++ tests/Unit/LatticeTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Unit;
+
+test("the lattice extension is available", function () {
+    expect(extension_loaded("lattice-php-ext"))->toBeTrue();
+});
