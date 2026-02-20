adc80d006cab5d513b07c4d0247d50783d37be6c
Mike Pepper <mdcpepper@gmail.com>
2026-02-20 06:03:16 +0000

    Add Save as New action to PromotionStack edit from (#27)

--- app/Filament/Admin/Resources/PromotionStacks/Pages/EditPromotionStack.php
+++ app/Filament/Admin/Resources/PromotionStacks/Pages/EditPromotionStack.php
@@ -5,8 +5,11 @@ namespace App\Filament\Admin\Resources\PromotionStacks\Pages;
 use App\Filament\Admin\Resources\PromotionStacks\Concerns\InteractsWithPromotionStackLayers;
 use App\Filament\Admin\Resources\PromotionStacks\PromotionStackResource;
 use App\Models\Promotions\PromotionStack;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Icons\Heroicon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
@@ -22,6 +25,14 @@ class EditPromotionStack extends EditRecord
     protected function getHeaderActions(): array
     {
         return [DeleteAction::make()];
+    }
+
+    protected function getFormActions(): array
+    {
+        $actions = parent::getFormActions();
+        array_splice($actions, 1, 0, [$this->saveAsNewAction()]);
+
+        return $actions;
     }
 
     /**
@@ -52,5 +63,45 @@ class EditPromotionStack extends EditRecord
 
             return $record->fresh(['layers']) ?? $record;
         });
+    }
+
+    protected function saveAsNewAction(): Action
+    {
+        return Action::make('saveAsNew')
+            ->label('Save as New')
+            ->icon(Heroicon::OutlinedDocumentDuplicate)
+            ->form([TextInput::make('name')->required()->maxLength(255)])
+            ->action(function (array $data): void {
+                /** @var PromotionStack $original */
+                $original = $this->record;
+
+                $newStack = DB::transaction(function () use (
+                    $data,
+                    $original,
+                ): PromotionStack {
+                    $stack = PromotionStack::query()->create([
+                        'team_id' => $this->currentTeamId(),
+                        'name' => $data['name'],
+                        'root_layer_reference' => null,
+                        'active_from' => null,
+                        'active_to' => null,
+                    ]);
+
+                    $formData = $this->populateLayerFormData($original, []);
+                    $formData['name'] = $data['name'];
+                    $formData['active_from'] = null;
+                    $formData['active_to'] = null;
+
+                    $this->syncStackGraph($stack, $formData);
+
+                    return $stack;
+                });
+
+                $this->redirect(
+                    PromotionStackResource::getUrl('edit', [
+                        'record' => $newStack,
+                    ]),
+                );
+            });
     }
 }
